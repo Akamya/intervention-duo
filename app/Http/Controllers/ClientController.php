fd3600154829dd1e96ffc969447c58641aceac2f
Akamya <elodie.langlet@gmail.com>
2024-12-06 17:04:58 +0100

    vue + controller clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = Client::with('tickets')->get();
+
+        return Inertia::render('Clients/Index', [
+            'clients' => $clients,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         return Inertia::render('Login');
-//     ])->name('Login');
-// });
 
 Route::get('/', function () {
     return Inertia::render('Auth/Login', [
         'canRegister' => Route::has('register'),
     ]);
 });
-
 
 Route::middleware([
     'auth:sanctum',
@@ -27,4 +19,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::get('/clients/index', [ClientController::class, 'index'])->name('clients.index');
 });
