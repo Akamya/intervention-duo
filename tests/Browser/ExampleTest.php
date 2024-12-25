841a2c85c7d9f11b195540caef8a2a202b117bb4
Qwest159 <duchesne-raphael@hotmail.com>
2024-12-25 16:23:05 +0100

    comment

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -8,61 +8,53 @@ use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 
-// test('visit site', function () {
-//     $this->browse(function (Browser $browser) {
-//         $browser->visit('/')
-//             ->screenshot('visit') // Sauvegarde la capture d'écran dans le dossier tests/Browser/screenshots
+test('visit site', function () {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+            ->screenshot('visit') // Sauvegarde la capture d'écran dans le dossier tests/Browser/screenshots
 
-//             ->assertSee('Email');
-//     });
-// });
+            ->assertSee('Email');
+    });
+});
 
-// test('connexion', function () {
-//     $user = User::factory()->create();
-//     $this->browse(function (Browser $browser) use ($user) {
+test('connexion', function () {
+    $user = User::factory()->create();
+    $this->browse(function (Browser $browser) use ($user) {
 
-//         $browser->loginAs($user)
-//             ->visit('/')
-//             ->screenshot('Dash') // Sauvegarde la capture d'écran dans le dossier tests/Browser/screenshots
-//             ->assertSee('Email');
-//     });
-// });
-
-
-// test('click modal', function () {
-//     $this->browse(function (Browser $browser) {
-//         // Crée un utilisateur pour le test
-//         $user = User::factory()->create();
-
-//         // Crée un client avec les données nécessaires
-//         // $client = Client::factory()->create([
-//         //     'nom' => 'Test Title',
-//         //     'prenom' => 'Bug Report',
-//         //     'email' => 'hdjd@kjfkf.jdd',
-//         //     'telephone' => 'hdjd@kjfkf.jdd',
-//         // ]);
-
-//         // Simulation de navigation et d'interaction
-//         $browser->loginAs($user) // Connexion avec l'utilisateur
-//             ->visit('/') // Page principale
-//             ->click('#create')
-//             // Clic sur le bouton de soumission
-//             ->waitForLocation('/clients/create')  // Attend 1 seconde
-//             ->type('#nom', "bfbfbfbf") // Remplit le champ "title"
-//             ->type('#prenom', "bvbvbvvbbv") // Remplit le champ "categorie"
-//             ->type('#email', "dingo@jjvf.jbbfdj") // Remplit le champ "commentaire"
-//             ->type('#telephone', "5455454") // Remplit le champ "commentaire"
-//             ->click('#submit-button') // Clic sur le bouton de soumission
-//             ->waitForLocation('/') // Attendre que la redirection vers la page des clients soit complète
+        $browser->loginAs($user)
+            ->visit('/')
+            ->screenshot('Dash') // Sauvegarde la capture d'écran dans le dossier tests/Browser/screenshots
+            ->assertSee('Email');
+    });
+});
 
 
-//             ->click('#open-modal-button') // Clic sur le bouton qui ouvre le modal
-//             ->pause(1000) // Pause pour s'assurer que le modal est complètement chargé
-//             ->screenshot('modal-opened') // Capture d'écran du modal
-//             ->assertSee('Clients') // Vérifie que le contenu attendu est visible
-//             ->screenshot('client-created'); // Capture d'écran après création
-//     });
-// });
+test('click modal', function () {
+    $this->browse(function (Browser $browser) {
+        // Crée un utilisateur pour le test
+        $user = User::factory()->create();
+
+        // Crée un client avec les données nécessaires
+        // $client = Client::factory()->create([
+        //     'nom' => 'Test Title',
+        //     'prenom' => 'Bug Report',
+        //     'email' => 'hdjd@kjfkf.jdd',
+        //     'telephone' => 'hdjd@kjfkf.jdd',
+        // ]);
+
+        // Simulation de navigation et d'interaction
+        $browser->loginAs($user) // Connexion avec l'utilisateur
+            ->visit('/') // Page principale
+            ->click('#create')
+            // Clic sur le bouton de soumission
+            ->waitForLocation('/clients/create')  // Attend 1 seconde
+            ->type('#nom', "bfbfbfbf") // Remplit le champ "title"
+            ->type('#prenom', "bvbvbvvbbv") // Remplit le champ "categorie"
+            ->type('#email', "dingo@jjvf.jbbfdj") // Remplit le champ "commentaire"
+            ->type('#telephone', "5455454") // Remplit le champ "commentaire"
+            ->click('#submit-button') // Clic sur le bouton de soumission
+            ->waitForLocation('/') // Attendre que la redirection vers la page des clients soit complète
+
 
             ->click('#open-modal-button') // Clic sur le bouton qui ouvre le modal
             ->pause(1000) // Pause pour s'assurer que le modal est complètement chargé
@@ -71,6 +63,12 @@ use Tests\DuskTestCase;
             ->screenshot('client-created'); // Capture d'écran après création
     });
 });
+
+            // ->click('#open-modal-button') // Clic sur le bouton qui ouvre le modal
+            // ->pause(1000) // Pause pour s'assurer que le modal est complètement chargé
+            // ->screenshot('modal-opened') // Capture d'écran du modal
+            // ->assertSee('Clients') // Vérifie que le contenu attendu est visible
+            // ->screenshot('client-created'); // Capture d'écran après création
 
 // test('non-admin cannot access technician page', function () {
 //     $this->browse(function (Browser $browser) {
@@ -87,5 +85,3 @@ use Tests\DuskTestCase;
 //             ->assertSee('Access denied'); // Vérifie qu'un message d'erreur s'affiche
 //     });
 // });
-
-
